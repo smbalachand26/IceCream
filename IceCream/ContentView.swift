86b6c6b8fd6f6167afe93c9bb8276da1c6204987
smbalachand26 <smbalachand26@blakeschool.org>
2023-05-20 17:19:14 -0500

    Initial Commit

---
+++ IceCream/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  IceCream
+//
+//  Created by Shruti Balachander26 on 5/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IceCream/IceCreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceCreamApp.swift
+//  IceCream
+//
+//  Created by Shruti Balachander26 on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct IceCreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
